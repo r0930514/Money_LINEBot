1843718489fc0a590332bbd1160cded4a123edf3
r0930514@gmail.com <r0930514@gmail.com>
2021-12-29 16:59:58 +0800

    保護Token

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const {GoogleSpreadsheet} = require('google-spreadsheet');
 const express = require('express');
 const { response } = require('express');
 const config = {
-    channelSecret: '05efa76a2722dec5b373cd6e1896e16e',
-    channelAccessToken: '10ilYqxZL97W8tazfF+xSLltH90SI/VeKGxg1Xaubd6m12t3bon415E1CS8ijdfDQxSc5yVPwktcIvHs3oyVyfdtyPu+0DeIy4GAOMSZkYGK0Rzsfwxa+DKvzBdDpYmNEeEu8sICmGJQchv4Se1exgdB04t89/1O/w1cDnyilFU='
+    channelSecret: '',
+    channelAccessToken: ''
 };
 const client = new linebot.Client(config);
 const app = express(); 
