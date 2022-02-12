f482a5b0bc26b6d6c1722fde1b95ef48ea725bec
r0930514@gmail.com <r0930514@gmail.com>
2022-02-12 23:20:26 +0800

    各函數模組化

--- main.js
+++ main.js
@@ -2,10 +2,7 @@
 const linebot = require('@line/bot-sdk');
 const express = require('express');
 const dataapi = require('./lib/api')
-const date = require('./lib/time');
 const config = require('./lib/config')
-
-
 
 //create a bot
 const client = new linebot.Client(config);
@@ -13,18 +10,33 @@ const client = new linebot.Client(config);
 //define express app
 const app = express(); 
 app.use('/static', express.static(__dirname+'/website'))
-app.use(express.json());
 app.use('/api', dataapi)
 
-
-
 //監聽webhook事件並執行相關動作
-app.post('/callback', linebot.middleware(config), (req, res)=>{
-    req.body.events.map((event)=>{
-        handleEvent(event);
-    })
-    res.sendStatus(200);
-})
+app.post('/callback', linebot.middleware(config), (req, res) => {
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).end();
+      });
+  });
+  
+  // event handler
+  function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      // ignore non-text-message event
+      return Promise.resolve(null);
+    }
+  
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+  
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+  }
+
 
 
 
@@ -34,17 +46,6 @@ app.get('/',(req,res)=>{
 });
 
 
-
-//當事件被觸發的動作
-function handleEvent(event){
-    if(event.type !== 'message' || event.message.type !== 'text'){
-        return null;
-    }
-
-    const echo = {type: 'text', text: event.message.text };
-    client.replyMessage(event.replyToken, echo);
-    return;
-}
 
 //在port監聽
 const port = process.env.PORT || 3000;
