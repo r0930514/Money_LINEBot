b9cbbebc36312d0dafffc3bb1eeaacdd7bcd3c24
r0930514 <r0930514@gmail.com>
2022-02-15 07:59:04 +0800

    新增說明

--- main.js
+++ main.js
@@ -56,6 +56,17 @@ app.post('/callback', linebot.middleware(config), (req, res) => {
           price: 100
         })   
         return client.replyMessage(event.replyToken, flex1)
+    case "help":
+        let echo = {type: 'text', text: `
+                輸入 早/中/晚餐：
+                    即可對應到加入50元/100元/100元的項目
+                快速新增一個項目：
+                    “價錢”（“新增項目名稱”
+                    範例：
+                        100（吃飯
+
+
+            `}
     default:
         const echo = { type: 'text', text: "不明" };
         return client.replyMessage(event.replyToken, echo)
